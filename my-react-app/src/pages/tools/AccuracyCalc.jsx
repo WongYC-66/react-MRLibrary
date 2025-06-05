d3683770e363077a07e25a678f3ea3c88bbf59a6
WongYC-66 <yengchien@hotmail.com>
2025-06-06 02:07:40 +0800

    fix: accuracy calc page for refactor

--- my-react-app/src/pages/tools/AccuracyCalc.jsx
+++ my-react-app/src/pages/tools/AccuracyCalc.jsx
@@ -1,4 +1,4 @@
-import { useSearchParams, Form, redirect, useLocation, NavLink, Link } from "react-router-dom"
+import { useSearchParams, Form, redirect, Link } from "react-router-dom"
 import { useState, useEffect } from "react"
 // 
 import FormBS from "react-bootstrap/Form"
@@ -7,15 +7,12 @@ import Table from "react-bootstrap/Table"
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 // 
 import { updatePagination } from "../../components/Pagination.jsx"
-import { renderImageWithMobId, filterMobList, updateSearchResultCount } from "../monster/utility.jsx"
-import { mapCategory, findMapCategoryByMapId } from "../map/utility.jsx"
-import data_mob from "../../../data/data_Mob.json"
-import data_mobStats from "../../../data/data_MobStats.json"
-
-import data_mapMobCount from "../../../data/data_MapMobCount.json"
-import data_mobMap from "../../../data/data_Mob_MapOnly.json"
+import { renderImageWithMobId, filterMobList, updateSearchResultCount, generateMobLibrary } from "../monster/utility.jsx"
+import { mapCategory } from "../map/utility.jsx"
 
 export default function AccuracyCalc() {
+    const [searchParams] = useSearchParams()
+
     const [playerStats, setPlayerStats] = useState({
         isMage: false,
         playerLevel: 1,
@@ -26,52 +23,8 @@ export default function AccuracyCalc() {
     const [mobLibrary, setMobLibrary] = useState({})
 
     useEffect(() => {
-        Object.entries(data_mob).forEach(([mobId, mobName]) => {
-            if (data_mobStats.hasOwnProperty(mobId)) {
-                data_mobStats[mobId] = { ...data_mobStats[mobId], name: mobName }
-            }
-        })
-
-        // HEAVY CALC MAPPING
-        const addMapCategoryToMobStats = () => {
-            const mapIdToCategory = {}  //  '100000000' => 'Henesys'
-
-            // data from inside data_MapMobCount (map.wz)
-            for (let mapId in data_mapMobCount) {
-                if (!(mapId in mapIdToCategory)) {
-                    mapIdToCategory[mapId] = findMapCategoryByMapId(mapId)
-                }
-
-                Object.keys(data_mapMobCount[mapId]).forEach(mobId => {
-                    if (!data_mobStats[mobId]) return
-                    if (!data_mobStats[mobId].mapCategory) {
-                        data_mobStats[mobId].mapCategory = new Set()
-                    }
-                    data_mobStats[mobId].mapCategory.add(mapIdToCategory[mapId])
-                })
-            }
-
-            // there is a problem, boss-type mob not inside data_MapMobCount
-            // combine data from monsterbook together then (string.wz)
-            // might have bugs for LKC mobs
-            for (let mobId in data_mobMap) {
-                if (!data_mobStats[mobId]) continue
-                data_mobMap[mobId].forEach(mapId => {
-                    if (!(mapId in mapIdToCategory)) {
-                        mapIdToCategory[mapId] = findMapCategoryByMapId(mapId)
-                    }
-
-                    if (!data_mobStats[mobId].mapCategory) {
-                        data_mobStats[mobId].mapCategory = new Set()
-                    }
-                    data_mobStats[mobId].mapCategory.add(mapIdToCategory[mapId])
-                })
-            }
-        }
-
-        addMapCategoryToMobStats()
-
-        setMobLibrary(data_mobStats)
+        const generatedMobLib = generateMobLibrary()
+        setMobLibrary(generatedMobLib)
     }, [])
 
     const magicAcc = getMagicalAccuracy(playerStats)
@@ -98,7 +51,7 @@ export default function AccuracyCalc() {
     }
     // console.log(playerStats)
 
-    const filteredMobList = filterMobList(mobLibrary)
+    const filteredMobList = filterMobList({ mobLibrary, searchParams })
 
     return (
         <div className="accuracy-tab d-flex flex-column">
