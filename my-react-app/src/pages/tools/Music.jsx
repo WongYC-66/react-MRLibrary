f81b3e2cc76a449ebb6c4078c241800f9b1dcd70
WongYC-66 <yengchien@hotmail.com>
2026-09-24 22:07:42 +0800

    revamp music page with playlist player

--- my-react-app/src/pages/tools/Music.jsx
+++ my-react-app/src/pages/tools/Music.jsx
@@ -1,30 +1,140 @@
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react";
 // 
-import FormBS from "react-bootstrap/Form"
+import Button from 'react-bootstrap/Button';
+import FormBS from "react-bootstrap/Form";
 import ListGroup from 'react-bootstrap/ListGroup';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
 // 
-import data_Music from "../../../data/data_Music.json"
+import data_Music from "../../../data/data_Music.json";
 
 export default function Music() {
-
+    // search & preview OST states
     const [searchText, setSearchText] = useState('')
-    const [selectedOST, setSelectedOST] = useState('')
+    const [previewOST, setPreviewOST] = useState('')
+
+    // playlist & playlist-player states
+    const [playlist, setPlaylist] = useState([
+        "AbandonedMine.mp3", "AcientForest.mp3", "amoria.mp3", "Anniv1.mp3", "Ariant.mp3"
+    ])
+    const [showPlayer, setShowPlayer] = useState(false);    // flag of whether to show
+    const [loopPlaylist, setLoopPlalist] = useState(false)  // flag of whether to loop
+    const [play, setPlay] = useState(false)                 // flag of whether to start/pause current playlist player
+    const [playIdx, setPlayIdx] = useState(-1)
+
+    // Ref to audio html element
+    const previewAudioRef = useRef(null)
+    const playlistAudioRef = useRef(null)
 
     const musicResult = Object.keys(data_Music).filter(name => name.toLowerCase().includes(searchText.toLowerCase()))
 
-    const handleMusicClick = (name) => {
-        setSelectedOST(name)
+    useEffect(() => {
+        // OST Preview 
+        if (!previewAudioRef.current) return
+        previewAudioRef.current.currentTime = 0;
+        if (previewOST === "") {
+            previewAudioRef.current.pause()     // press to pause
+        } else {
+            previewAudioRef.current.play()      // pres to play preview
+        }
+    }, [previewOST]);
+
+    useEffect(() => {
+        // Playlist
+        if (!playlistAudioRef.current) {
+            return
+        }
+        if (!play || playIdx === -1) {
+            playlistAudioRef.current.pause()     // press to pause
+        } else {
+            playlistAudioRef.current.play()      // pres to play preview
+        }
+    }, [play, playIdx]);
+
+
+
+    const handlePreviewClick = (name) => {
+        if (previewOST === name) {
+            setPreviewOST("") // same song clicked twice, clear
+        } else {
+            setPreviewOST(name) // new preview clicked
+        }
     }
 
-    // 
-    const OST_URL = `https://github.com/scotty66f/mapleroyals_library_related/raw/refs/heads/main/audio/${selectedOST}`
-    const ALL_OST_URL = 'https://minhaskamal.github.io/DownGit/#/home?url=https:%2F%2Fgithub.com%2Fscotty66f%2Fmapleroyals_library_related%2Ftree%2Fmain%2Faudio'
+    const handleAddToMenuClick = (name) => {
+        if (playlist.includes(name)) return
+        let newPlaylist = [...playlist]
+        newPlaylist.push(name)
+        setPlaylist(newPlaylist)
+    }
+
+    const removeFromPlaylist = (idx) => {
+        let newPlaylist = [...playlist]
+        newPlaylist.splice(idx, 1)
+        setPlaylist(newPlaylist)
+        let nextIdx = Math.min(newPlaylist.length - 1, playIdx)
+        setPlayIdx(nextIdx)
+    }
+
+    const sortPlaylist = () => {
+        let newPlaylist = [...playlist]
+        newPlaylist.sort((a, b) => a.localeCompare(b))
+        setPlaylist(newPlaylist)
+    }
+
+    const shufflePlaylist = () => {
+        let newPlaylist = [...playlist]
+        newPlaylist.sort((a, b) => Math.random() - 0.5)
+        setPlaylist(newPlaylist)
+    }
+
+    const togglePlayMusic = () => {
+        setPlay(prev => !prev)
+    }
+
+    const toggleLoopPlaylist = () => {
+        setLoopPlalist(prev => !prev)
+    }
+
+    const startPlaylistPlayer = () => {
+        setPlayIdx(0)
+        setShowPlayer(true)
+        setPlay(true)
+    }
+
+    const stopPlaylistPlayer = () => {
+        setPlayIdx(-1)
+        setShowPlayer(false)
+        setPlay(false)
+    }
+
+    const playlistGoPrev = () => {
+        setPlayIdx(prevIdx => Math.max(0, prevIdx - 1))
+    }
+
+    const playlistGoNext = () => {
+        let nextIdx = playIdx + 1
+        if (nextIdx === playlist.length) {
+            // reach ending
+            if (loopPlaylist) {
+                nextIdx = 0     // go back to first song if loop
+            } else {
+                nextIdx -= 1    // no loop, hold at last song
+            }
+        }
+        setPlayIdx(nextIdx)
+    }
+
+    const OST_SOURCE_URL = "https://github.com/scotty66f/mapleroyals_library_related/raw/refs/heads/main/audio/"
+    const PREVIEW_OST_URL = `${OST_SOURCE_URL}${previewOST}`        // e.g. "https://github.com/scotty66f/mapleroyals_library_related/raw/refs/heads/main/audio/amoria.mp3"
+    const PLAYLIST_OST_URL = `${OST_SOURCE_URL}${playlist[playIdx]}`         // e.g. "https://github.com/scotty66f/mapleroyals_library_related/raw/refs/heads/main/audio/amoria.mp3"
 
     return (
-        <div className="music d-flex p-3">
+        <div className="music d-flex flex-column flex-md-row p-3 gap-3">
 
             {/* Left window */}
-            <div className="d-flex flex-column" style={{ width: '250px' }}>
+            <div className="d-flex flex-column w-100">
                 {/* Search Input */}
                 <FormBS.Control
                     className=""
@@ -42,33 +152,164 @@ export default function Music() {
                 {/* List of Music */}
                 <div className="mt-3" style={{ maxHeight: '60vh', overflowY: 'auto' }}>
                     <ListGroup variant="flush">
-                        {musicResult.map(name =>
-                            <ListGroup.Item action key={name} onClick={() => handleMusicClick(name)} active={name === selectedOST}>
-                                {name.replace('.mp3', '')}
+                        {musicResult.map(name => {
+                            let isPlayingThisSongPreview = name === previewOST
+                            let ostUrl = `${OST_SOURCE_URL}/${name}`
+                            return <ListGroup.Item key={name} className="d-flex align-center justify-content-between">
+                                {/* Text */}
+                                <p className="m-0 p-0">{name.replace('.mp3', '')}</p>
+
+                                <div className="d-flex gap-3">
+                                    {/* Preview Button */}
+                                    {!isPlayingThisSongPreview && <IconToolTip text={"Preview"}>
+                                        <i className="bi bi-play-circle" style={{ cursor: "pointer" }} onClick={() => handlePreviewClick(name)}></i>
+                                    </IconToolTip>
+                                    }
+                                    {isPlayingThisSongPreview && <IconToolTip text={"Pause"}>
+                                        <i className="bi bi-pause-circle" style={{ cursor: "pointer" }} onClick={() => handlePreviewClick(name)}></i>
+                                    </IconToolTip>
+                                    }
+
+                                    {/* Add to playlist Button */}
+                                    <IconToolTip text={"Add to playlist"}>
+                                        <i className="bi bi-folder-plus" style={{ cursor: "pointer" }} onClick={() => handleAddToMenuClick(name)}></i>
+                                    </IconToolTip>
+
+
+                                    {/* Download button */}
+                                    <a href={ostUrl} download={ostUrl}>
+                                        <IconToolTip text={"Download"}>
+                                            <i className="bi bi-download"></i>
+                                        </IconToolTip>
+                                    </a>
+                                </div>
                             </ListGroup.Item>
-                        )}
+                        })}
                     </ListGroup>
+
+                    {/* Invisible preview music player */}
+                    <div className=''>
+                        <audio ref={previewAudioRef} src={PREVIEW_OST_URL}></audio>
+                    </div>
                 </div>
             </div>
 
-            {/* Right window */}
-            {selectedOST &&
-                <div className='ms-3 w-50'>
-                    <figure>
-                        <figcaption>Listen to {selectedOST}:</figcaption>
-                        <audio controls src={OST_URL} loop></audio>
-                        <p>
-                            <a href={OST_URL} download={OST_URL} className="btn btn-primary mt-2"> Download audio </a>
-                        </p>
-                    </figure>
+            {/* Right window - Playlist */}
+            <div className="w-100">
+                <h4>Current Playlist</h4>
 
-                    {/* Download All */}
-                    <p>
-                        <a href={ALL_OST_URL} download={ALL_OST_URL} className="btn btn-primary mt-2"> Download All (360 MB) </a>
-                    </p>
+                <div className="mt-2 d-flex justify-content-end gap-3">
+                    {/* Sort button */}
+                    <IconToolTip text={"sort"}>
+                        <Button variant="outline-light" onClick={() => sortPlaylist()}><i className="bi bi-sort-alpha-down"></i></Button>
+                    </IconToolTip>
+
+                    {/* shuffle button */}
+                    <IconToolTip text={"shuffle"}>
+                        <Button variant="outline-light" onClick={() => shufflePlaylist()}><i className="bi bi-shuffle"></i></Button>
+                    </IconToolTip>
+
+                    {/* loop button */}
+                    <IconToolTip text={"loop"}>
+                        <Button variant={loopPlaylist ? "light" : "outline-light"} onClick={() => toggleLoopPlaylist()}><i className="bi bi-repeat"></i></Button>
+                    </IconToolTip>
+
+                    {/* Start play playlist button */}
+                    {!showPlayer &&
+                        <IconToolTip text={"start"}>
+                            <Button variant="outline-light" onClick={() => startPlaylistPlayer()}><i className="bi-play"></i></Button>
+
+                        </IconToolTip>
+                    }
+                    {showPlayer &&
+                        <IconToolTip text={"End"}>
+                            <Button variant="light" onClick={() => stopPlaylistPlayer()}><i className="bi-pause"></i></Button>
+                        </IconToolTip>
+                    }
                 </div>
-            }
 
+                {/* Playlist */}
+                <ListGroup variant="flush" className="mt-3" style={{ maxHeight: '60vh', overflowY: 'auto' }}>
+                    {playlist.map((ost, idx) => {
+                        let name = ost.replace(".mp3", "")
+                        {/* playlist - row item */ }
+                        return <ListGroup.Item key={ost} className="d-flex align-items-center justify-content-between m-1 py-2 border rounded-2 gap-3">
+
+                            {/* row - left */}
+                            <div className="d-flex align-items-center gap-3">
+                                <img className="rounded-1" style={{ height: '40px' }} src={`https://ui-avatars.com/api/?name=${name}&background=random`}></img>
+                                {name}
+                            </div>
+
+                            {/* row - right */}
+                            <div>
+                                {/* X button */}
+                                <IconToolTip text={"Remove"}>
+                                    <Button variant="outline-light" onClick={() => removeFromPlaylist(idx)}><i className="bi bi-trash"></i></Button>
+                                </IconToolTip>
+                            </div>
+
+                        </ListGroup.Item >
+                    })}
+                </ListGroup>
+            </div>
+
+
+            {/* Bottom Playlist Player */}
+            <Offcanvas
+                show={showPlayer}
+                onHide={stopPlaylistPlayer}
+                placement="bottom"
+                scroll={true}
+                backdrop={false}
+                className="bg-dark"
+                style={{ height: "15vh" }}
+                onEntered={() => {
+                    if (play && playlistAudioRef.current) {
+                        playlistAudioRef.current.play().catch(err => {
+                            console.log("play failed:", err);
+                        });
+                    }
+                }}>
+                <Offcanvas.Header closeButton className="d-flex justify-content-between bg-dark text-white" closeVariant="white">
+                    {/* OST name being played */}
+                    <h5><i className="bi bi-music-note"></i> {playlist[playIdx]}</h5>
+
+
+                </Offcanvas.Header>
+                <Offcanvas.Body className="d-flex justify-content-center align-items-center gap-3 overflow-hidden">
+
+                    {/* Playlist Control < || > */}
+                    {/* < */}
+                    <Button variant="outline-light" onClick={() => playlistGoPrev()}><i className="bi bi-skip-backward-fill"></i></Button>
+
+                    {/* Play or Pause */}
+                    {!play && <Button variant="outline-light" onClick={() => togglePlayMusic()}><i className="bi-play"></i></Button>}
+                    {play && <Button variant="light" onClick={() => togglePlayMusic()}><i className="bi-pause"></i></Button>}
+
+                    {/* > */}
+                    <Button variant="outline-light" onClick={() => playlistGoNext()}><i className="bi bi-skip-forward-fill"></i></Button>
+
+                    {/* Invisible playlist music player */}
+                    <div>
+                        <audio controls style={{ maxHeight: "35px" }} ref={playlistAudioRef} src={PLAYLIST_OST_URL} onEnded={() => playlistGoNext()} ></audio>
+                    </div>
+                </Offcanvas.Body>
+            </Offcanvas>
         </div>
     )
 }
+
+function IconToolTip({ text, children }) {
+    return (
+        <OverlayTrigger placement="top" delay={{ show: 350, hide: 400 }} overlay={(props) => renderTooltip(props, text)}>
+            {children}
+        </OverlayTrigger>
+    )
+}
+
+const renderTooltip = (props, text) => (
+    <Tooltip {...props}>
+        {text}
+    </Tooltip>
+);
